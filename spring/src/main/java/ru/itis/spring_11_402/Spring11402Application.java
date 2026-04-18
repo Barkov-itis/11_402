2c75e0d0fd0f8a5dc85276a70b8b13f85993a8ec
danil-Itis <tron5268@mail.ru>
2026-04-18 07:33:18 +0300

    api + slf4j
    17/04

--- spring/src/main/java/ru/itis/spring_11_402/Spring11402Application.java
+++ spring/src/main/java/ru/itis/spring_11_402/Spring11402Application.java
@@ -2,6 +2,8 @@ package ru.itis.spring_11_402;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Spring11402Application {
@@ -10,4 +12,8 @@ public class Spring11402Application {
         SpringApplication.run(Spring11402Application.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ spring/src/main/java/ru/itis/spring_11_402/services/CurrencyRateLoader.java
@@ -0,0 +1,5 @@
+package ru.itis.spring_11_402.services;
+
+public interface CurrencyRateLoader {
+    String getCurrencyRates();
+}
